794e0cb5b2753174aa2f6298d1ddf4477a74eceb
korey506 <ugbanaadebisi@yahoo.com>
2024-06-06 19:24:25 -0700

    113-bst_search.c

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,20 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * array_to_avl - builds an AVL tree from an array
- * @array: array to create from
- * @size: size of the array
- *
- * Return: a pointer to the root node of the created AVL tree
+ * array_to_bst - builds a Binary Search Tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size: the number of elements in the array
+ * Return: A pointer to the root node of the created BST
  *         NULL on failure
  */
-avl_t *array_to_avl(int *array, size_t size)
+bst_t *array_to_bst(int *array, size_t size)
 {
 	unsigned int i;
-	avl_t *root = NULL;
+	bst_t *root = NULL;
 
 	for (i = 0; i < size; i++)
-		avl_insert(&root, array[i]);
+		bst_insert(&root, array[i]);
 
 	return (root);
 }
